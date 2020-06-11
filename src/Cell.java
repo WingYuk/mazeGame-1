c95ba04a8e2feef670f2bc6d63c0d734c9949f65
WingYuk <66694296+WingYuk@users.noreply.github.com>
2020-06-10 23:33:34 -0700

    Separated Cell class from Maze

---
+++ src/Cell.java
@@ -0,0 +1,18 @@
+package UI;
+
+/**
+ * I separated the Cell from the maze
+ * because I need to use the Cell class
+ * in class Player, Monster and Power.
+ *
+ * Also I have asked Victor, he said it
+ * will be a better idea to separate them
+ */
+
+
+public class Cell {
+    public int x;
+    public int y;
+    public int value;
+    public boolean visited = false;
+}
